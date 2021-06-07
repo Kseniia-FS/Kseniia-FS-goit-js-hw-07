46adccdc87077e6c4d863f1dec491ed1e2db72db
Kseniia <ksuyhaboss@gmail.com>
2021-06-07 09:42:16 +0300

    add changes in task 2 and 6

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,18 @@
 const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
 ];
 
-const list = document.querySelector('#ingredients');
+const list = document.querySelector("#ingredients");
 
-const elements = ingredients => {
-  return ingredients.map(ingredient => {
-    const ingredientEl = document.createElement('li');
-    ingredientEl.textContent = ingredient;
-    list.append(ingredientEl);
-  });
-};
+const elements = ingredients.map((ingredient) => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.textContent = ingredient;
+  return ingredientEl;
+});
 
-elements(ingredients);
+list.append(...elements);
--- js/task-6.js
+++ js/task-6.js
@@ -1,21 +1,10 @@
 const input = document.querySelector("#validation-input");
 
 input.addEventListener("input", isInputValid);
-input.addEventListener("blur", onInputBlur);
 
 function isInputValid(event) {
   const currentLength = event.currentTarget.value.length;
   const dataLength = Number(input.dataset.length);
-  if (currentLength === dataLength) {
-    input.classList.remove("invalid");
-    input.classList.add("valid");
-  }
-  if (currentLength !== dataLength) {
-    input.classList.remove("valid");
-    input.classList.add("invalid");
-  }
-}
-function onInputBlur() {
   if (currentLength === dataLength) {
     input.classList.remove("invalid");
     input.classList.add("valid");
